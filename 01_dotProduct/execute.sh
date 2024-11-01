b08879812b784e6639091fd665df8dc140b3fec3
giuseppe <gambacortagiuseppe@outlook.it>
2024-11-01 16:22:05 +0100

    dot

---
+++ 01_dotProduct/execute.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <number_of_cores>"
+    exit 1
+fi
+
+NUMBER_OF_CORE=$1
+files=("omp-dot_parallelForReduce.c" "omp-dot_parallel_reduce.c" "omp-dot_parallel_atomic.c" "omp-dot_parallel_critical.c")
+
+for file in "${files[@]}"; do
+    gcc -std=c99 -Wall -Wextra -pedantic -fopenmp "$file" -o "${file%.c}"
+done
+
+for file in "${files[@]}"; do
+    echo "Running ${file%.c} with $NUMBER_OF_CORE cores"
+    OMP_NUM_THREADS=$NUMBER_OF_CORE ./"${file%.c}"
+done
---
+++ 01_dotProduct/omp-dot_parallelForReduce.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <omp.h>
+
+void fill( int *v1, int *v2, size_t n )
+{
+    const int seq1[3] = { 3, 7, 18};
+    const int seq2[3] = {12, 0, -2};
+    for (size_t i=0; i<n; i++) {
+        v1[i] = seq1[i%3];
+        v2[i] = seq2[i%3];
+    }
+}
+
+int dot(const int *v1, const int *v2, size_t n)
+{
+    int result = 0;
+
+    #pragma omp parallel for reduction(+:result)
+    for (size_t i = 0; i < n; i++) {
+        result += v1[i] * v2[i];
+    }
+
+    return result;
+}
+
+int main( int argc, char *argv[] )
+{
+    size_t n = 512*1024*1024l; /* array length */
+    const size_t n_max = 512*1024*1024l; /* max length */
+    int *v1, *v2;
+
+    if ( argc > 2 ) {
+        fprintf(stderr, "Usage: %s [n]\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    if ( argc > 1 ) {
+        n = atol(argv[1]);
+    }
+
+    if ( n > n_max ) {
+        fprintf(stderr, "FATAL: Array too long (requested length=%lu, maximum length=%lu\n", (unsigned long)n, (unsigned long)n_max);
+        return EXIT_FAILURE;
+    }
+
+    printf("Initializing array of length %lu\n", (unsigned long)n);
+    printf("num of thread %d\n", omp_get_max_threads());
+    v1 = (int*)malloc( n*sizeof(v1[0])); assert(v1 != NULL);
+    v2 = (int*)malloc( n*sizeof(v2[0])); assert(v2 != NULL);
+    fill(v1, v2, n);
+
+    const int expect = (n % 3 == 0 ? 0 : 36);
+
+    const double tstart = omp_get_wtime();
+    const int result = dot(v1, v2, n);
+    const double elapsed = omp_get_wtime() - tstart;
+
+    if ( result == expect ) {
+        printf("Test OK\n");
+    } else {
+        printf("Test FAILED: expected %d, got %d\n", expect, result);
+    }
+    printf("Elapsed time: %f\n", elapsed);
+    free(v1);
+    free(v2);
+
+    return EXIT_SUCCESS;
+}
